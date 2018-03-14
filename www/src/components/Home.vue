3cb8508e4fc0720ccfd0838f7f78dd47250b10ad
jordanengstrom <jordan.engstrom@gmail.com>
2018-03-14 14:58:08 -0600

    styled buttons

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -1,30 +1,65 @@
 <template>
     <div class="home">
-        <h1>How would you like to pair your wine?</h1>
-        <button type="button" class="btn btn-primary btn-lg btn-block">By Food</button>
-        <button type="button" class="btn btn-primary btn-lg btn-block">By Sweetness</button>
-        <button type="button" class="btn btn-primary btn-lg btn-block">By Pet</button>
+        <div class="title">
+            <h1>How would you like to pair your wine?</h1>
+        </div>
+        <button type="button" class="btn btn-primary btn-lg btn-block btn-top">By Food</button>
+        <button type="button" class="btn btn-primary btn-lg btn-block btn-middle">By Sweetness</button>
+        <button type="button" class="btn btn-primary btn-lg btn-block btn-bottom">By Pet</button>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data() {
-      return {
+    export default {
+        name: 'Home',
+        data() {
+            return {
 
-      }
+            }
+        }
     }
-  }
 </script>
 
 <style>
-h1 {
-    margin-bottom: 5vh;
-}
-.btn-block {
-    width: 100%;
-    height: 20vh;
-    margin-bottom: 5vh;
-}
+    .btn-block {
+        width: 80%;
+        height: 20vh;
+        margin-bottom: 5vh;
+        border: none;
+        opacity: .85;
+    }
+
+    .btn-top {
+        background-color: #E1CEB1;
+    }
+
+    .btn-middle {
+        background-color: #CCA272;
+    }
+
+    .btn-bottom {
+        background-color: #9D5E3D;
+    }
+
+    .btn:hover {
+        background-color: #572E3C;
+    }
+
+    .title {
+        background-color: rgba(255, 255, 255, 0.75);
+        margin-bottom: 5vh;
+        margin-top: 5vh
+    }
+
+    .home {
+        background-image: url('../assets/corks.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 100%;
+        margin: 0 0rem !important;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center
+    }
 </style>
--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
-        
+        <nav class="navbar">
+            <div class="flexor">
+                <a class="navbar-brand" href="#">Vine</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Your Corkboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">You</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
 </template>
 
 <script>
+    export default {
+        name: 'Navbar',
+        data() {
+            return {
 
+            }
+        }
+    }
 </script>
 
-<style>
-
+<style scoped>
+    .flexor {
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-around;
+    }
 </style>
