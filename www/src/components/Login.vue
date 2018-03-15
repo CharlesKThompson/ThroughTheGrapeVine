ef378b4b023f7262ef8ab32a789928f90b6365da
Nic <nicjorgi@gmail.com>
2018-03-15 09:24:49 -0600

    Login Style

--- www/src/components/Login.vue
+++ www/src/components/Login.vue
@@ -1,52 +1,57 @@
 <template>
     <div class="flexor">
-    <div class="login-page">
-        <div>
-            <h1>
-                Welcome to ThroughTheGrapeVine
-            </h1>
-        </div>
-        <div>
-            <h2>Please login to continue</h2>
-        </div>
-        <div v-if="loginForm == true" class="login">
-            <div class="login-box">
-                <div class="row login-credentials">
-                    <div class="col-sm-12 login-form">
-                        <form @submit.prevent="login">
-                            <div class="form-group">
-                                <input v-model="user.email" type="text" name="email" placeholder="email" >
-                                <input v-model="user.password" type="password" name="password" placeholder="password">
+        <div class="login-page flexor2">
+            <div class="bg-1">
+                <div>
+                    <h1>
+                        Welcome to ThroughTheGrapeVine
+                    </h1>
+                </div>
+                <div>
+                    <h2>Please login to continue.</h2>
+                </div>
+                <div v-if="loginForm == true" class="login">
+                    <div class="login-box">
+                        <div class="row login-credentials">
+                            <div class="col-sm-12 login-form">
+                                <form @submit.prevent="login">
+                                    <div class="form-group flexor2">
+                                        <input v-model="user.email" type="text" name="email" placeholder="email" class="form-control">
+                                        <input v-model="user.password" type="password" name="password" placeholder="password" class="form-control">
+                                    </div>
+                                    <div class="col-sm-12 login-button">
+                                        <button type="submit" class="btn btn-success">Login</button>
+                                    </div>
+                                </form>
                             </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn btn-success" @click="loginForm = !loginForm">Create an account</button>
+                    </div>
+                </div>
+
+                <div v-if="loginForm == false" class="create-account-box">
+                    <div class="createUser">
+                        <div class="col-sm-12 create-form">
+                            <form @submit.prevent="create" class="flexor2">
+                                <input v-model="user.username" type="text" name="username" placeholder="username" class="form-control">
+                                <input v-model="user.email" type="text" name="email" placeholder="email" class="form-control">
+                                <input v-model="user.password" type="password" name="password" placeholder="password" class="form-control">
+                                <input v-model="user.password" type="password" name="password" placeholder="confirm password" class="form-control">
                                 <div class="col-sm-12 login-button">
-                                <button type="submit" class="btn btn-success">Login</button>
-                            </div>
-                        </form>
+                                    <button type="submit" class="btn btn-success login-button">Create</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn btn-success" @click="loginForm = !loginForm">Back to Login</button>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div v-if="loginForm == false" class="create-account-box">
-            <div class="createUser">
-                <div class="col-sm-12 create-form">
-                    <form @submit.prevent="create">
-                        <input v-model="user.username" type="text" name="username" placeholder="username">
-                        <input v-model="user.email" type="text" name="email" placeholder="email">
-                        <input v-model="user.password" type="password" name="password" placeholder="password">
-                        <input v-model="user.password" type="password" name="password" placeholder="confirm password">
-                        <div class="col-sm-12 login-button">
-                            <button type="submit" class="btn btn-success login-button">Create</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div>
-            <button class="btn btn-success" @click="loginForm = !loginForm">Create an account</button>
-        </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -80,8 +85,26 @@
         margin-bottom: 1rem;
     }
 
+    .form-control {
+        margin-top: 1vh
+    }
+
+    .bg-1 {
+        background-color: rgba(175, 111, 175, 0.6);
+        padding: 20px;
+        border-radius: 30px;
+        color: ivory
+    }
+
     .flexor {
         align-items: center;
+    }
+
+    .flexor2 {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center
     }
 
     .login-button {
