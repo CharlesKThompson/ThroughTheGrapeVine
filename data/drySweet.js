e70904ce8f6a26a7ab76c6b8692bd55bed41ed76
Dustin Hofstetter <Hofstetter.dustinart@gmail.com>
2018-03-15 16:26:06 -0600

    data push

---
+++ data/drySweet.js
@@ -0,0 +1,21 @@
+var drySweet = {
+    "class" : [
+        {
+        "type" : "Dry",
+        "sugars" : "0-6 sugar calories",
+        "description" : "Dry Red and White Wines, Lambrusco Secco, Demi-Sec Vouvray",
+        "type" : "Off-Dry",
+        "sugars" : "6-12 sugar calories",
+        "description" : "Extra-Dry Champagne, Lambrusco Secco, Demi-Sec Vouvray",
+        "type" : "Medium Dry/Semi-Sweet",
+        "sugar" : "12-30 sugar calories",
+        "description" : "Sweet Riesling, Demi-Sec Champagne, Dry Champagne, Demi-Sec Vouvray, Sercial Madeira, Lambrusco Semisecco, Lambrusco Amabile",
+        "type" : "Medium Sweet",
+        "sugars" : "30-72 sugar calories",
+        "description" : "Sweet Riesling, Port, Madeira, Brachetto D'Acqui, Moscato D'Asti & Muscat De Beaumes-De-Venise, Lambrusco Dolce, Recioto Della Valpolicella",
+        "type" : "Sweet/Very Sweet",
+        "sugars" : "Over 72 sugar calories",
+        "description" : "German TBA Riesling & Eiswein, Rutherglen Muscat, Tokaji Aszu, Sauternes, Recioto Della Valpolicella",
+        }
+    ]
+}
