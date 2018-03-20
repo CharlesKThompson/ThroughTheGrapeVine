9cfcf946a108abc154837b4d187c1f6f06da9b32
jordanengstrom <jordan.engstrom@gmail.com>
2018-03-19 19:47:29 -0600

    vw and uw comments

--- server/server-assets/routes/comments.js
+++ server/server-assets/routes/comments.js
@@ -3,9 +3,11 @@ var Lists = require('../models/list');
 var Comments = require('../models/comment');
 var Users = require('../models/user');
 var userWines = require('../models/userWine');
+var VineyardWines = require('../models/lightwine');
 
+// region VINEYARDWINE COMMENTS
 // GET ALL COMMENTS BY USER WINE
-router.get('/lists/:listId/wines/:wineId/comments', (req, res, next) => {
+router.get('/lists/:listId/vineyardwines/:wineId/comments', (req, res, next) => {
     Comments.find({wineId: req.params.wineId }) 
         .then(comments => {
             res.send(comments);
@@ -14,7 +16,7 @@ router.get('/lists/:listId/wines/:wineId/comments', (req, res, next) => {
 });
 
 // ADD COMMENT TO USER WINE
-router.post('/lists/:listId/wines/:wineId/comments', (req, res, next) => {
+router.post('/lists/:listId/vineyardwines/:wineId/comments', (req, res, next) => {
     req.body.listId = req.params.listId;
     req.body.userId = req.session.uid;
     req.body.wineId = req.params.wineId;
@@ -26,7 +28,7 @@ router.post('/lists/:listId/wines/:wineId/comments', (req, res, next) => {
 });
 
 // EDIT COMMENT BY COMMENT ID
-router.put('/lists/:listId/wines/:wineId/comments/:commentId', (req, res, next) => {
+router.put('/lists/:listId/vineyardwines/:wineId/comments/:commentId', (req, res, next) => {
     Comments.findByIdAndUpdate(
         req.params.commentId,
         req.body,
@@ -39,14 +41,61 @@ router.put('/lists/:listId/wines/:wineId/comments/:commentId', (req, res, next) 
 });
 
 // DELETE COMMENT BY COMMENT ID
-router.delete('/lists/:listId/wines/:wineId/comments/:commentId', (req, res, next) => {
+router.delete('/lists/:listId/vineyardwines/:wineId/comments/:commentId', (req, res, next) => {
     Comments.findById(req.params.commentId)
     .then(comments => {
         comments.remove();
-        return console.log("Comment successfully deleted!");
+        return console.log("Comment on vineyard wine successfully deleted!");
     })
     .catch(next);
 });
 
+// endregion VINEYARDWINE COMMENTS
+
+// region USERWINE COMMENTS
+router.get('/lists/:listId/userwines/:wineId/comments', (req, res, next) => {
+    Comments.find({wineId: req.params.wineId }) 
+        .then(comments => {
+            res.send(comments);
+        })
+        .catch(next);
+});
+
+// ADD COMMENT TO USER WINE
+router.post('/lists/:listId/userwines/:wineId/comments', (req, res, next) => {
+    req.body.listId = req.params.listId;
+    req.body.userId = req.session.uid;
+    req.body.wineId = req.params.wineId;
+    Comments.create(req.body)
+        .then(comment => {
+            res.send(comment);
+        })
+        .catch(next)
+});
+
+// EDIT COMMENT BY COMMENT ID
+router.put('/lists/:listId/userwines/:wineId/comments/:commentId', (req, res, next) => {
+    Comments.findByIdAndUpdate(
+        req.params.commentId,
+        req.body,
+        { new: true },
+        (err, log) => {
+            if (err) return res.status(500).send(err);
+            return res.send(log);
+        })
+        .catch(next);
+});
+
+// DELETE COMMENT BY COMMENT ID
+router.delete('/lists/:listId/userwines/:wineId/comments/:commentId', (req, res, next) => {
+    Comments.findById(req.params.commentId)
+    .then(comments => {
+        comments.remove();
+        return console.log("Comment on user wine successfully deleted!");
+    })
+    .catch(next);
+});
+
+// endregion USERWINE COMMENTS
 
 module.exports = router;
