047f22a73d7394216a544d40249e5134dc84243a
Nic <nicjorgi@gmail.com>
2018-03-30 15:10:22 -0600

    Made things straight

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -6,24 +6,21 @@
                     <img src="../assets/grapes.png" alt="grapes" height="50px">
                 </router-link>
             </div>
-            <div>
-                <div class="row">
-
-                    <router-link :to="{name: 'UserSearch'}">
-                        <i class="fas fa-search fa-2x"></i>
-                    </router-link>
-                    <router-link :to="{name: 'Corkboard'}">
-                        <img src="../assets/corks.png" alt="cork" height="50px">
-                    </router-link>
-                    <div class="dropdown pointer">
-                        <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span>
-                                <i class="fas fa-user fa-2x"></i>
-                            </span>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                            <button @click.prevent="logout" class="dropdown-item pointer" type="button">logout</button>
-                        </div>
+            <div class="d-flex">
+                <router-link :to="{name: 'UserSearch'}">
+                    <i class="fas fa-search fa-2x"></i>
+                </router-link>
+                <router-link :to="{name: 'Corkboard'}">
+                    <img src="../assets/corks.png" alt="cork" height="50px">
+                </router-link>
+                <div class="dropdown pointer">
+                    <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span>
+                            <i class="fas fa-user fa-2x"></i>
+                        </span>
+                    </a>
+                    <div class="dropdown-menu text-center" aria-labelledby="dropdownMenu2">
+                        <button @click.prevent="logout" class="dropdown-item pointer" type="button">Logout</button>
                     </div>
                 </div>
             </div>
@@ -61,5 +58,22 @@
         display: flex;
         align-items: flex-end;
         justify-content: space-between;
+    }
+
+    .dropdown {
+        cursor: pointer;
+    }
+
+    .dropdown-menu {
+        left: -7rem;
+        cursor: pointer;
+    }
+
+    .pointer {
+        cursor: pointer;
+    }
+
+    .d-flex {
+        align-items: center
     }
 </style>
