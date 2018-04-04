a4a54253abac5321e1fe2f9fad5ab5b87dbb9535
Nic <nicjorgi@gmail.com>
2018-04-04 16:50:58 -0600

    Styled UserWines on Lists

--- www/src/components/UserWine.vue
+++ www/src/components/UserWine.vue
@@ -1,34 +1,43 @@
 <template>
-    <div class="userwine">
+    <div class="userwine col-sm-12">
         <div class="row flexor">
             <div class="col-sm-12">
                 <h4 class="name">{{userwine.brandName}}</h4>
                 <p>({{userwine.variety}})</p>
             </div>
-            <div v-if="show == false" @click="show = true" class="pointer">
+            <div v-if="show == false" @click="show = true" class="pointer col-sm-12">
                 <i class="fas fa-chevron-circle-down"></i>
             </div>
             <div v-if="show == true">
-                <div @click="show = false" class="pointer">
-                    <i class="fas fa-chevron-circle-up"></i>
-                </div>
-                <div class="col-sm-12">
-                    <p>{{userwine.description}}</p>
-                </div>
-                <div class="col-sm-12">
-                    <div class="row">
-                        <div class="col-sm-5">
-                            <p>Type: {{userwine.type}}</p>
-                            <p>Price:{{userwine.price}}</p>
-                        </div>
-                        <div class="col-sm-5">
-                            <p>Pairings: {{userwine.pairings}}</p>
-                            <p>Recipes: {{userwine.recipes}}</p>
-                        </div>
+                <div class="row">
+                    <div @click="show = false" class="pointer col-sm-12">
+                        <i class="fas fa-chevron-circle-up"></i>
                     </div>
-                </div>
-                <div v-if="userwine.userId == user._id">
-                    <button @click="deleteUW(userwine)" class="btn btn-link">Delete wine from List</button>
+                    <div class="col-sm-12">
+                        <img :src="userwine.img" class="wine" alt="wine">
+                    </div>
+                    <div class="col-sm-12">
+                        <p>{{userwine.description}}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4>Type:</h4>
+                        <p>{{userwine.type}}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4>Price:</h4>
+                        <p>{{userwine.price}}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4>Pairings:</h4>
+                        <p>{{userwine.pairings}}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4>Recipes:</h4>
+                        <p>{{userwine.recipes}}</p>
+                    </div>
+                    <div v-if="userwine.userId == user._id" class="col-sm-12">
+                        <button @click="deleteUW(userwine)" class="btn btn-link">Delete wine from List</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -83,8 +92,7 @@
 <style scoped>
     .flexor {
         display: flex;
-        flex-direction: column;
-        align-items: center
+        flex-direction: column
     }
 
     .name {
@@ -114,5 +122,11 @@
     .pointer:hover {
         opacity: 1;
         transition: linear .3s all
+    }
+
+    .wine {
+        width: 80%;
+        border: solid 1px rgba(87, 46, 60, 0.726);
+        border-radius: 10px;
     }
 </style>
